23c160ad1260cd17feb396447421e79235b5191f
garyfostergh <garyfoster@cox.net>
2017-04-12 05:54:46 -0400

    it works!!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { UserService } from './shared/user.service';
+import { Component, OnInit } from '@angular/core';
+import { User } from './shared/user';
 
 @Component({
   selector: 'aap-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'aap works!';
+export class AppComponent implements OnInit {
+  title = 'ROOMS';
+  user: User;
+  
+  constructor(private userService: UserService) {
+
+  }
+
+  ngOnInit() {
+    this.userService.user$.subscribe(user => this.user = user);
+  }
+
+  login() {
+    console.log("login button pressed");
+    this.userService.login();
+  }
+
+  logout() {
+    console.log("logout button pressed");
+    this.userService.logout();
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,57 @@
+import { UserService } from './shared/user.service';
+import { RoomService } from './room/room.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+import { MaterialModule } from '@angular/material';
+import 'hammerjs';
+import { AngularFireModule, AuthMethods, AuthProviders } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { RoomListComponent } from './room-list/room-list.component';
+import { RoomComponent } from './room/room.component';
+
+const firebaseDatabaseConfig = {
+  apiKey: "AIzaSyBIEjsWly6mOrEaewEGNwLxFceo7Id7EBY",
+  authDomain: "fourrooms-cce77.firebaseapp.com",
+  databaseURL: "https://fourrooms-cce77.firebaseio.com",
+  storageBucket: "fourrooms-cce77.appspot.com",
+  messagingSenderId: "321050506384"
+};
+
+const firebaseAuthConfig = {
+  provider: AuthProviders.Google,
+  method: AuthMethods.Redirect
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    RoomListComponent,
+    RoomComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    AngularFireModule.initializeApp(firebaseDatabaseConfig, firebaseAuthConfig),
+    HttpModule,
+    MaterialModule.forRoot(),
+    RouterModule.forRoot([
+      { path: "welcome", component: WelcomeComponent },
+      {
+        path: "room", children: [
+          { path: "", component: RoomListComponent },
+          { path: ":name", component: RoomComponent }
+        ]
+      },
+      { path: "**", redirectTo: "welcome" }
+    ])
   ],
-  providers: [],
+  providers: [RoomService, UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
